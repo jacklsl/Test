50b592d8cdc2e06fe9bba3f4d7ccffab8e2496fc
jacklsl <songlin885X@gmail.com>
2023-09-08 09:37:34 +0800

    快速排序算法

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,40 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+
 using namespace std;
-int main()
-{
-    cout<<"Hello world"<<endl;
-    return 0;
+
+void quickSort(vector<int>& nums, int left, int right) {
+  if (left >= right) {
+    return;
+  }
+
+  int pivot = nums[left + (right - left) / 2];
+  int i = left, j = right;
+  while (i <= j) {
+    while (nums[i] < pivot) {
+      i++;
+    }
+    while (nums[j] > pivot) {
+      j--;
+    }
+    if (i <= j) {
+      swap(nums[i], nums[j]);
+      i++;
+      j--;
+    }
+  }
+
+  quickSort(nums, left, j);
+  quickSort(nums, i, right);
+}
+
+int main() {
+  vector<int> nums = {10, 7, 8, 9, 1, 5};
+  quickSort(nums, 0, nums.size() - 1);
+  for (int i = 0; i < nums.size(); i++) {
+    cout << nums[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
 }
